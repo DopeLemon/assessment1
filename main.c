074a39ad100dd711438fc44c34034bbb14db1041
Liam O'Neill <liam.j.oneill126@yahoo.com.au>
2019-04-02 23:03:40 +0000

    bunch of printf statements asking the user what they would like the program to do

--- main.c
+++ main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 
+//Inputs: 
+//Outputs: encryption, decryption  
+
+int func (int , int , int );	//prototype
+
 int main()
 {
-    printf("test");
+ 
+printf("Would you like to: \n\n");
+
+printf("1.  Encrypt a message using a rotation cipher given message text AND the rotation amount?\n\n");
+printf("                                                     OR                                        \n\n");
+printf("2.  Decrypt a message encrypted with a rotation cipher given cipher text AND rotation amount?\n\n");
+printf("                                                     OR                                        \n\n");
+printf("3.  Encrypt of a message using a substitution cipher given message text AND alphabet substitutions?\n\n");
+printf("                                                     OR                                        \n\n");
+printf("4.  Decrypt a message encrypted with a substitution cipher given cipher text AND substitutions?\n\n");
+printf("                                                     OR                                        \n\n");
+printf("5.  Decrypt a message encrypted with a rotation cipher given ONLY cipher text?\n\n");
+printf("                                                     OR                                        \n\n");
+printf("6.  Decrypt a message encrypted with a substitution cipher given ONLY cipher text?\n\n");
+
+
+
+  
+    
 
     return 0;
 }
