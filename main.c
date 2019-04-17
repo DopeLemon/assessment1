006f5363b3aada2cb46451cf01b2fb4dea08fd6f
Liam O'Neill <liam.j.oneill126@yahoo.com.au>
2019-04-17 02:12:05 +0000

    first 2 tasks commented out

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-//The following code can encrypt or decrypt both a rotation cipher and a substitution cipher
+ //The following code can encrypt or decrypt both a rotation cipher and a substitution cipher
 
 
 #include <stdio.h>
@@ -20,8 +20,8 @@ int main() {
 char Substitution[] = "QWERTYUIOPASDFGHJKLZXCVBNM";
 
 
-printf("Would you like to: \n\n"); //lists the task that are able to be performed 
-
+printf("Would you like to: \n\n");                                    //lists the task that are able to be performed 
+                                                                      
 printf("1.  Encrypt a message using a rotation cipher given message text AND the rotation amount?\n\n");
 printf("2.  Decrypt a message encrypted with a rotation cipher given cipher text AND rotation amount?\n\n");
 printf("3.  Encrypt of a message using a substitution cipher given message text AND alphabet substitutions?\n\n");
@@ -31,7 +31,7 @@ printf("6.  Decrypt a message encrypted with a substitution cipher given ONLY ci
 
 printf("Please choose a task and enter the number which corresponds to it above: \n\n");
 
-scanf("%d", &A);                                              //get the user to enter a value for the task
+scanf("%d", &A);                                                    //get the user to enter a value for the task
 
 
 switch (A)
@@ -39,66 +39,76 @@ switch (A)
     
     
 case 1:
- printf("1: Enter a word or phrase to be encrypted: ");                   //the user is prompted to enter the word or phrase 
+ printf("1: Enter a word or phrase to be encrypted: ");             //the user is prompted to enter the word or phrase 
         scanf(" %[^\n]s", string);                                  //this is scanned into the string 
-    printf("Enter the key: ");                            //the user is prompted to enter the letter shift amount (key)
-        scanf("%d", &KEY);                                     //this is scanned and stored into the KEY value 
-  for ( i = 0; i < strlen(string); i++)                                     //the conditions for the encryption to work
+    printf("Enter the key: ");                                      //the user is prompted to enter the letter shift amount (key)
+        scanf("%d", &KEY);                                          //this is scanned and stored into the KEY value 
+  for ( i = 0; i < strlen(string); i++)                             //the conditions for the encryption to work
     {
-       X = string[i] - 65;  
-       if (string[i]<65 || string[i]>122)
+       X = string[i] - 65;                                          //converts ASCII value of letter to alphabet position of letter and stores this value in the letter X
+       if (string[i]<65 || string[i]>122)                           //code checks if the ASCII value of the letter lies outside upper case A to lower case z
  {
    
-     message[i]= ' ';
-                                                        //adds 65 to the new value to shift the letter back into the ASCII range
+     message[i]= ' ';                                               //if the above statement is true, the console will print a space
+                                                       
  }
-   else if(X> -1 && X < 26) 
+   else if(X> -1 && X < 26)                                        //code checks if the letters entered are in upper case 
   {
-      X=((X+KEY)%26)+65;                   //shifts the value according to the key value   
-      message[i]= X; 
-}
-else 
+    //if the above statement is true the following steps execute
+      X= X+KEY;                                                    //KEY is added to the alphabet position of the letter
+      X= X%26;                                                     //modulus of this number is calculated
+      X= X+65;                                                     //65 is added to the number found through the modulus operator to shift the letter back into the ASCII range 
+      message[i]= X;                                               //this converted letter is stored in the string array message[i]
+}   
+ else                                                              //if the letter isn't in upper case then the following steps execute 
     {
-        X= (((X-32)+KEY)%26)+65;
-        
-         message[i]= X;
+        X= X-32;                                                   //this shifts the ASCII value of a lower case letter to the corresponding upper case letter value in the range of 0 to 25
+        X= X+KEY;                                                  //KEY is added to the alphabet position of the upper case letter
+        X= X%26;                                                   //modulus of this number is calculated
+        X= X+65;                                                   //adds 65 to the new value to shift the letter back into the ASCII range
+        message[i]= X;                                             //this converted letter is stored in the string array message[i]
      }
 }
    
-   printf("Therefore the new phrase is: \n\%s\n\n", message); //the new phrase/word is printed
+   printf("Therefore the new phrase is: \n\%s\n\n", message);     //the new phrase/word is printed in all upper case
  
  break;
  
  
  
  case 2:
- printf("2: Enter a word or phrase to be decrypted: ");                   //the user is prompted to enter the word or phrase 
-        scanf(" %[^\n]s", string);                                  //this is scanned into the string 
-    printf("Enter the key: ");                            //the user is prompted to enter the letter shift amount (key)
-        scanf("%d", &KEY);                                     //this is scanned and stored into the KEY value
-  for ( i = 0; i < strlen(string); i++)                                     //the conditions for the encryption to work
+ printf("2: Enter a word or phrase to be decrypted: ");          //the user is prompted to enter the word or phrase 
+        scanf(" %[^\n]s", string);                               //this is scanned into the string 
+    printf("Enter the key: ");                                   //the user is prompted to enter the letter shift amount (key)
+        scanf("%d", &KEY);                                       //this is scanned and stored into the KEY value
+  for ( i = 0; i < strlen(string); i++)                          //the conditions for the encryption
     {
-     X = (string[i] - 65 ) ;                          //shifts the value according to the key value
-     if(string[i]<65 || string[i]>122)
+     X = string[i] - 65;                                         //converts ASCII value of letter to alphabet position of letter and stores this value in the letter X
+     if(string[i]<65 || string[i]>122)                           //code checks if the ASCII value of the letter lies outside upper case A to lower case z
  {
    
-     message[i]= ' ';
-                                                        //adds 65 to the new value to shift the letter back into the ASCII range
+     message[i]= ' ';                                            //if the above statement is true, the console will print a space
+                                                        
  }
-  else if(X> -1 && X < 26)
+  else if(X> -1 && X < 26)                                      //code checks if the letters entered are in upper case 
+  
   {
-      X=((X+(26 - KEY))%26)+65;                   //shifts the value according to the key value
-        message[i]= X; 
+      X= X+(26 - KEY);                                          //KEY is added to the alphabet position of the letter (26-KEY so it shifts the letter forwards and keeps the value positive)
+      X= X%26;                                                  //modulus of this number is calculated
+      X= X+65;                                                  //adds 65 to the new value to shift the letter back into the ASCII range
+      message[i]= X;                                            //this converted letter is stored in the string array message[i]
 }                    
-else
+else                                                            //if the letter isn't in upper case then the following steps execute 
     {
-        X= ((((X-32)+(26-KEY))%26)+65);         message[i]= X;
+       X= X-32;                                                 //this shifts the ASCII value of a lower case letter to the corresponding upper case letter value in the range of 0 to 25
+       X= X+(26-KEY);                                           //KEY is added to the alphabet position of the letter (26-KEY so it shifts the letter forwards and keeps the value positive)
+       X= X%26;                                                 //modulus of this number is calculated
+       X= X+65;                                                 //adds 65 to the new value to shift the letter back into the ASCII range
+       message[i]= X;                                           //this converted letter is stored in the string array message[i]
      }
+ }
 
-                                                        //adds 65 to the new value to shift the letter back into the ASCII range
-                                                 //assigns the value X to the new letter 
-   }
-  printf("Therefore the new phrase is: \n\%s\n\n", message); //the new phrase/word is printed
+ printf("Therefore the new phrase is: \n\%s\n\n", message);    //the new phrase/word is printed
   break;
   
   
