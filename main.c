c72163eaf9f938fd0621ac244e2f1995f52bbb16
Liam O'Neill <liam.j.oneill126@yahoo.com.au>
2019-04-29 02:34:25 +0000

    small changes

--- main.c
+++ main.c
@@ -31,7 +31,7 @@ int main() {
     char X;
     char string[8888];
     char message[8888];
-    char Substitution[26]= 
+    char Substitution[26]; 
     char Alphabet[]="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 
@@ -40,9 +40,9 @@ int main() {
     printf("1.  Encrypt a message using a rotation cipher given message text AND the rotation amount?\n\n");
     printf("2.  Decrypt a message encrypted with a rotation cipher given cipher text AND rotation amount?\n\n");
     printf("3.  Encrypt of a message using a substitution cipher given message text AND alphabet substitutions?\n\n");
-    printf("4.  Decrypt a message encrypted with a substitution cipher given cipher text AND substitutions?\n\n");
-    /*printf("5.  Decrypt a message encrypted with a rotation cipher given ONLY cipher text?\n\n");
-    printf("6.  Decrypt a message encrypted with a substitution cipher given ONLY cipher text?\n\n\n\n");*/
+    //printf("4.  Decrypt a message encrypted with a substitution cipher given cipher text AND substitutions?\n\n");
+    //printf("5.  Decrypt a message encrypted with a rotation cipher given ONLY cipher text?\n\n");
+    //printf("6.  Decrypt a message encrypted with a substitution cipher given ONLY cipher text?\n\n\n\n");*/
 
     printf("Please choose a task and enter the number which corresponds to it above: \n\n");
 
@@ -61,26 +61,26 @@ int main() {
         for ( i = 0; i < strlen(string); i++)                           //the conditions for which the encryption will work
         {
             X = string[i] - 65;                                         //converts ASCII value of letter to alphabet position of letter and stores this value in the letter X
+            
             if(string[i]>64 && string[i]<91)                            //code checks if the ASCII value of the letter lies outside upper case A to lower case z
             {
                 //if the above statement is true the following steps execute
-                X= X+KEY;                                               //KEY is added to the alphabet position of the letter
+                X= X+KEY;                                               //KEY is added to the alphabet position of the letter to give the letter the key shift
                 X= X%26;                                                //modulus of this number is calculated
                 X= X+65;                                                //65 is added to the number found through the modulus operator to shift the letter back into the ASCII range
                 message[i]= X;                                          //this converted letter is stored in the string array message[i]
             }    
-                                                        
-
             
-           else if(string[i]>96 && string[i]<123)                       //checks if the letters entered are in lowercase                                        
+            else if(string[i]>96 && string[i]<123)                       //checks if the letters entered are in lowercase                                        
             {
                 //if the above statement is true the following steps execute
                 X= X-32;                                                //this shifts the ASCII value of a lower case letter to the corresponding upper case letter value in the range of 0 to 25
-                X= X+KEY;                                               //KEY is added to the alphabet position of the letter
+                X= X+KEY;                                               //KEY is added to the alphabet position of the letter to give the letter the key shift
                 X= X%26;                                                //modulus of this number is calculated
                 X= X+65;                                                //65 is added to the number found through the modulus operator to shift the letter back into the ASCII range
                 message[i]= X;                                          //this converted letter is stored in the string array message[i]
             }
+            
             else                                                        //if the letter isn't in upper case then the following steps execute
             {
                 message[i] = string[i];                                 //If the value of the input is neither an upper or lowercase letter, it is not encrypted
@@ -101,10 +101,11 @@ int main() {
        for ( i = 0; i < strlen(string); i++)                           //the conditions for which the encryption will work
         {
             X = string[i] - 65;                                        //converts ASCII value of letter to alphabet position of letter and stores this value in the letter X
+            
             if(string[i]>64 && string[i]<91)                           //code checks if the ASCII value of the letter lies outside upper case A to lower case z
             {
                 //if the above statement is true the following steps execute
-                X= X+(26-KEY);                                         //KEY is added to the alphabet position of the letter
+                X= X+(26-KEY);                                         //KEY is added to the alphabet position of the letter to give the letter the key shift
                 X= X%26;                                               //modulus of this number is calculated
                 X= X+65;                                               //65 is added to the number found through the modulus operator to shift the letter back into the ASCII range
                 message[i]= X;                                         //this converted letter is stored in the string array message[i]
@@ -114,11 +115,12 @@ int main() {
             {
                 //if the above statement is true the following steps execute
                 X= X-32;                                               //this shifts the ASCII value of a lower case letter to the corresponding upper case letter value in the range of 0 to 25
-                X= X+(26-KEY);                                         //KEY is added to the alphabet position of the letter
+                X= X+(26-KEY);                                         //KEY is added to the alphabet position of the letter to give the letter the key shift
                 X= X%26;                                               //modulus of this number is calculated
                 X= X+65;                                               //65 is added to the number found through the modulus operator to shift the letter back into the ASCII range
                 message[i]= X;                                         //this converted letter is stored in the string array message[i]
             }
+            
             else                                                       //if the letter isn't in upper case then the following steps execute
             {
                 message[i] = string[i];                                //If the value of the input is neither an upper or lowercase letter, it is not encrypted
@@ -130,32 +132,29 @@ int main() {
 
 
 
-
-
     case 3:
         printf("3; Substitution cipher encryption: Enter a word or phrase to be encrypted: ");    //the user is prompted to enter the word or phrase
         scanf(" %[^\n]s", string);                                     //this is scanned into the string
         printf("Enter the 26 capital letter substitution key with no spaces: ");  //the user is prompted to enter the letter substitution key
         scanf("%s", Substitution);                                    //this is scanned and stored into the substitution array
-        for ( i = 0; i < strlen(string); i++) {                       //the conditions for the encryption
+        for ( i = 0; i < strlen(string); i++)                         //the conditions for the encryption
+        {                       
             X = string[i] - 65;                                       //converts ASCII value of letter to alphabet position of letter and stores this value in the letter X
 
 
-            if(string[i]>64 && string[i]<91) {                        //code checks if the letters entered are in upper case
+            if(string[i]>64 && string[i]<91)                          //code checks if the letters entered are in upper case
+            {                        
                 X= Substitution[X];                                   //the letter X is assigned to the letter value of position X along the substitution key array
                 message[i]= X;                                        //this converted letter is stored in the string array message[i]
             }
-
-
 
             else if(string[i]>96 && string[i]<123)                    //checks if the letters entered are in lowercase
             {
                 X= (string[i] - 65) -32;                              //this shifts the ASCII value of a lower case letter to the corresponding upper case letter value in the range of 0 to 25
                 X= Substitution[X];                                   //the letter X is assigned to the letter value of position X along the substitution key array
                 message[i]= X;                                        //this converted letter is stored in the string array message[i]
-
             }
-
+            
             else 
 			{
                 message[i]= string[i];                                //all other values are kept the same (i.e. are not encrypted and are printed as the plain text)
@@ -167,7 +166,7 @@ int main() {
         break;
 
 
-    case 4:
+    /*case 4:
         printf("4 Substitution cipher decryption; Enter a word or phrase to be decrypted: ");           //the user is prompted to enter the word or phrase
         scanf(" %[^\n]s", string);                                 //this is scanned into the string
         printf("Enter the 26 capital letter substitution key with no spaces: ");  //the user is prompted to enter the letter substitution key
@@ -190,7 +189,7 @@ int main() {
 
          printf("Therefore the new phrase is: \n\%s\n\n", message); 
 
-    break;
+    break;*/
 
 
     default:
